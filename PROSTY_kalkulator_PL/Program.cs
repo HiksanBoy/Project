75bb9c5a6259907ed59fd2d8676f55dd1aae5286
Andrzej <andrzej.p@tutanota.com>
2020-04-03 22:18:56 +0200

    Dodana opcja czyszczenia pamięci kalkulatora w menu.

--- PROSTY_kalkulator_PL/Program.cs
+++ PROSTY_kalkulator_PL/Program.cs
@@ -8,54 +8,71 @@ namespace PROSTY_kalkulator_PL
 {
     class Program
     {
+        
         static void Main(string[] args)
+            
         {
-            //Deklaracja zmiennych
-            int liczba1, liczba2;
-
-            //Pwitanie 
-            Console.WriteLine("Witaj w prostym kalkulatorze :)");
-            Console.WriteLine("#################################\n");
-
-            //Zapytanie o "liczba1"
-            Console.WriteLine("Wprowadź pierwszą liczbę i potwierdź wybór Enterem");
-            liczba1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("----------------------------------------------------");
-
-            //Zapytanie o "liczba2"
-            Console.WriteLine("\nWprowadź drugą liczbę i potwierdź wybór Enterem");
-            liczba2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("----------------------------------------------------");
-
-            //Zapytanie o operaceje na liczbach
-            Console.WriteLine("Wybierz operacje którą chcesz wykonać na wcześniej wybranych liczbach:\n");
-            Console.WriteLine("\ta - dodawanie (+)\n \tb - odejmowanie (-)\n \tc - mnożenie(*)\n \td - dzielenie(/)\n");
-            Console.WriteLine("Jaki jest twój wybór?");
-
-
-            // Meny wyboru 
-            switch (Console.ReadLine())
+            int exit = 1;
+            do
             {
-                case "a":
+                //Deklaracja zmiennych
+                int liczba1, liczba2;
+                
 
-                    Console.WriteLine($"Twój wynik {liczba1} + {liczba2} = "+(liczba1 + liczba2));
-                    break;
-                case "b":
+                //Pwitanie 
+                Console.WriteLine("Witaj w prostym kalkulatorze :)");
+                Console.WriteLine("#################################\n");
 
-                    Console.WriteLine($"Twój wynik {liczba1} - {liczba2} = " + (liczba1 - liczba2));
-                    break;
+                //Zapytanie o "liczba1"
+                Console.WriteLine("Wprowadź pierwszą liczbę i potwierdź wybór Enterem");
+                liczba1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("----------------------------------------------------");
 
-                case "c":
+                //Zapytanie o "liczba2"
+                Console.WriteLine("\nWprowadź drugą liczbę i potwierdź wybór Enterem");
+                liczba2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("----------------------------------------------------");
 
-                    Console.WriteLine($"Twój wynik {liczba1} * {liczba2} = " + (liczba1 * liczba2));
-                    break;
+                //Zapytanie o operaceje na liczbach
+                Console.WriteLine("Wybierz operacje którą chcesz wykonać na wcześniej wybranych liczbach:\n");
+                Console.WriteLine("\ta - dodawanie (+)\n \tb - odejmowanie (-)\n \tc - mnożenie(*)\n \td - dzielenie(/)\n \tclear - WYCZYŚĆ PAMIĘĆ \n");
+                Console.WriteLine("Jaki jest twój wybór?");
 
-                case "d":
 
-                    Console.WriteLine($"Twój wynik {liczba1} / {liczba2} = " + (liczba1 / liczba2));
-                    break;
-                                       
-            }
+                // Meny wyboru 
+                switch (Console.ReadLine())
+                {
+                    case "a":
+
+                        Console.WriteLine($"Twój wynik {liczba1} + {liczba2} = " + (liczba1 + liczba2));
+                        break;
+                    case "b":
+
+                        Console.WriteLine($"Twój wynik {liczba1} - {liczba2} = " + (liczba1 - liczba2));
+                        break;
+
+                    case "c":
+
+                        Console.WriteLine($"Twój wynik {liczba1} * {liczba2} = " + (liczba1 * liczba2));
+                        break;
+
+                    case "d":
+
+                        Console.WriteLine($"Twój wynik {liczba1} / {liczba2} = " + (liczba1 / liczba2));
+                        break;
+
+                    case "clear":
+                        Console.WriteLine("Kasowanie" + (exit = 0));
+                        Console.Clear();
+                        break;
+                        
+                                               
+                }
+            } while (exit < 1);
+
+
+
+
 
             //Zakończenie 
             Console.WriteLine("\n\nAby zakończyć program naciśnij dowolny ");
